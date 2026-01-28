acecb73883d366de31f31ef3fa553bf31e9862b5
zlc1004 <lucaszhang1001@gmail.com>
2026-01-28 14:33:16 -0800

    feat(config): add base game config and support for multiple games

--- config.py
+++ config.py
@@ -1,13 +1,29 @@
-class Kahoot:
+class BaseGameConfig:
+    uri: str = ""
+    code_input_xpath: str = ""
+    submit_code_button_selector: str = ""
+    nickname_input_xpath: str = ""
+    submit_nickname_button_selector: str = ""
+
+class Kahoot(BaseGameConfig):
     uri="https://kahoot.it"
     code_input_xpath="//*[@id=\"game-input\"]"
     submit_code_button_selector = "xpath=/html/body/div/div[1]/div/div/div[1]/div/div[2]/div[2]/main/div/form/button"
     nickname_input_xpath="//*[@id=\"nickname\"]"
     submit_nickname_button_selector = "xpath=/html/body/div/div[1]/div/div/div[1]/div/div[2]/main/div/form/button"
 
-class MyShortAnswer:
+class MyShortAnswer(BaseGameConfig):
     uri="https://app.myshortanswer.com/join"
     code_input_xpath="//*[@id=\":r1:\"]"
-    submit_code_button_selector = "xpath="
+    submit_code_button_selector = "xpath=/html/body/div/div/div/form/button"
     nickname_input_xpath="//*[@id=\"text-input-go-input\"]"
-    submit_nickname_button_selector = "xpath="
+    submit_nickname_button_selector = "xpath=/html/body/div/form/div/div/div/button"
+
+class Blooket(BaseGameConfig):
+    pass
+
+supported_games = {
+    "kahoot": Kahoot,
+    "myshortanswer": MyShortAnswer,
+    "blooket": Blooket
+}
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import string
 import asyncio
 from patchright.async_api import async_playwright
 
-from config import Kahoot as config
+import config
+
+game=config.BaseGameConfig
 
 def generate_hex_string(length):
     return ''.join(random.choices(string.hexdigits, k=length)).lower()
@@ -14,23 +16,23 @@ async def run_client(join_code, browser):
         page = await browser.new_page()
         await page.goto("https://kahoot.it")
 
-        await page.wait_for_selector(f"xpath={config.code_input_xpath}", timeout=60000)
-        await page.locator(f"xpath={config.code_input_xpath}").fill(join_code)
+        await page.wait_for_selector(f"xpath={game.code_input_xpath}", timeout=60000)
+        await page.locator(f"xpath={game.code_input_xpath}").fill(join_code)
         await page.wait_for_timeout(500)
 
-        await page.locator(config.submit_code_button_selector).click()
+        await page.locator(game.submit_code_button_selector).click()
         await page.wait_for_timeout(500)
 
-        await page.wait_for_selector(f"xpath={config.nickname_input_xpath}")
+        await page.wait_for_selector(f"xpath={game.nickname_input_xpath}")
 
         random_hex_1 = generate_hex_string(5)
         random_hex_2 = generate_hex_string(5)
         text_to_type = f"{random_hex_1} {random_hex_2}"
 
-        await page.locator(f"xpath={config.nickname_input_xpath}").fill(text_to_type)
+        await page.locator(f"xpath={game.nickname_input_xpath}").fill(text_to_type)
         await page.wait_for_timeout(500)
 
-        await page.locator(config.submit_nickname_button_selector).click()
+        await page.locator(game.submit_nickname_button_selector).click()
         await page.wait_for_timeout(500)
     except Exception as e:
         print(f"An error occurred in a client: {e}")
@@ -39,6 +41,12 @@ async def run_client(join_code, browser):
             await page.close()
 
 async def main():
+    global game
+    game_choice = input(f"Please select a game from {','.join(config.supported_games.keys())}: ").strip().lower()
+    game = config.supported_games.get(game_choice)
+    if not game:
+        print("Unsupported game selected.")
+        return
     join_code = input("Please enter the code to join: ")
     num_clients_str = input("Please enter the number of clients to run: ")
     try:
